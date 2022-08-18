2e42901364855dc9a1c6b40631afd97f9b96fb92
zituhossain <zituhossain@gmail.com>
2022-08-18 13:59:29 +0600

    fetch data from API

--- src/App.js
+++ src/App.js
@@ -1,23 +1,80 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { useState, useEffect } from "react";
+
+const URL = "https://rest-api-without-db.herokuapp.com/users/";
 
 function App() {
+  const [users, setUsers] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  const getAllUsers = () => {
+    fetch(URL)
+      .then((res) => {
+        if (!res.ok) {
+          throw Error("could not fetch");
+        }
+        return res.json();
+      })
+      .then((data) => {
+        setUsers(data.users);
+      })
+      .catch((err) => {
+        setError(err.message);
+      })
+      .finally(() => {
+        setIsLoading(false);
+      });
+  };
+
+  useEffect(() => {
+    getAllUsers();
+  }, []);
+
+  // delete user
+  const handleDelete = (id) => {
+    fetch(URL + `/${id}`, {
+      method: "DELETE",
+    })
+      .then((res) => {
+        if (!res.ok) {
+          throw Error("could not delete");
+        }
+        getAllUsers();
+      })
+
+      .catch((err) => {
+        setError(err.message);
+      });
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>User Management App</h1>
+      {isLoading && <h2>Loading...</h2>}
+      {error && <h2>{error}</h2>}
+
+      <section>
+        {users &&
+          users.map((user) => {
+            const { id, username, email } = user;
+            return (
+              <article key={id} className="card">
+                <p>{username}</p>
+                <p>{email}</p>
+                <button className="btn">Edit</button>
+                <button
+                  className="btn"
+                  onClick={() => {
+                    handleDelete(id);
+                  }}
+                >
+                  Delete
+                </button>
+              </article>
+            );
+          })}
+      </section>
     </div>
   );
 }
